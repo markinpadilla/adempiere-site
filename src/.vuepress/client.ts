809f8f8542fd5bbbf9cfa04f839d0c127bbd6689
markinpadilla <126710318+markinpadilla@users.noreply.github.com>
2023-03-01 21:26:13 +0000

    Deploying to gh-pages from @ markinpadilla/adempiere-site@ebc85217238e38602bd7a501a70d6d31117100af 🚀

--- src/.vuepress/client.ts
+++
@@ -1,9 +0,0 @@
-// .vuepress/client.ts
-import { defineClientConfig } from "@vuepress/client";
-import Releases from "./components/Releases.vue";
-
-export default defineClientConfig({
-  enhance: ({ app, router, siteData }) => {
-    app.component("Releases", Releases);
-  },
-});
